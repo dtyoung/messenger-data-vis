a854f099901d48b89d6b3ea7d2a8d0ed00572b31
dtyoung <daniel10young@gmail.com>
2020-08-09 21:22:57 +1200

    Add ESLint to server project

--- server/src/App.ts
+++ server/src/App.ts
@@ -1,10 +1,12 @@
-import express, { Request, Response } from "express";
-const app = express()
+import express, { Request, Response } from 'express';
 
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).send("Hello World!");
-})
+const app = express();
 
-app.listen(8000,()=>{
-  console.log('Server Started at Port, 8000')
-})
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).send('Hello World!');
+});
+
+app.listen(8000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server Started at Port, 8000');
+});
